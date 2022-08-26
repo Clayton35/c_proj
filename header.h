d53b976245dd52720928afbd8009e39584547553
Clayton35 <73616310+Clayton35@users.noreply.github.com>
2022-08-26 14:20:43 -0600

    Create header.h

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include<stdlib.h>
+#include<stdio.h>
+
+#define SIZE 4
+#define WIDE 20
+
+void getStrings(char inputStr[], char revStr[]);
+void reverseStr(char inputStr[], char revStr[]);
+void isPalindrome(char inputStr[], char revStr[], int count);
+
+
+#endif
