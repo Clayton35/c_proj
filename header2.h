22884f6a00b79fa31d7fbed3dddd00f02464ab46
Clayton35 <73616310+Clayton35@users.noreply.github.com>
2022-08-26 14:21:07 -0600

    Create header2.h

---
+++ header2.h
@@ -0,0 +1,17 @@
+#ifndef HEADER2_H
+#define HEADER2_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+#define SIZE 30
+
+void getStr(char inputStr[]);
+void convertStr(char inputStr[], int length);
+void randChars(char randStr[]);
+void challenge(int value);
+void checkAns(char inputStr2[], int pick);
+
+#endif
